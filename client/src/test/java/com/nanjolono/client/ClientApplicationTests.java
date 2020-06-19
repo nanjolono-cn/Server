6aba83a279fc6241307a9c3f53610d6632d7fd03
nanjolono <nanjolono@outlook.com>
2020-06-19 08:47:27 +0800

    initial project

---
+++ client/src/test/java/com/nanjolono/client/ClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nanjolono.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
